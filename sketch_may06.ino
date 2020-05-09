b6678d5a34ff8c7a6ea06abcb180d68cd9eef7ed
sunny03-choi <62876483+sunny03-choi@users.noreply.github.com>
2020-05-10 00:21:54 +0900

    Update sketch_may06.ino

--- sketch_may06.ino
+++ sketch_may06.ino
@@ -14,8 +14,7 @@ int pump =8;
 void setup() {
   
  Serial.begin(9600);
-  lcd.begin(16,2);
-  lcd.clear();
+ 
   pinMode (pump,OUTPUT);
   
   
@@ -32,40 +31,57 @@ void loop() {
   lcd.backlight();
   Serial.print("read soil : ");
   Serial.println(soil);
+  lcd.begin(16,2);
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.home();
   lcd.print("read soil : ");
   lcd.println(soil);
-  if (soil >430) {
+  delay(700);
+  if (soil >618) {
     Serial.println ("vary dry");
     Serial.println ("Pumping 1 second");
+    lcd.noDisplay();
+    delay(800);
+    lcd.display();
     lcd.setCursor(0,1);
     lcd.println(" dry Pumping");
+    delay(900);
     digitalWrite(pump,HIGH);
     delay(1000);
+    digitalWrite(pump,LOW);
   }
   else {
     Serial.println(" vary wet");
-    lcd.println(" Wet");
+    lcd. setCursor(0,1);
+    lcd.print(" Wet");
+    delay(100);
     digitalWrite(pump, LOW);
   }
- if (soil <350)
+ if (soil <400) {
  digitalWrite(green, HIGH);
+ delay(500);
+ digitalWrite(green,LOW);
+ delay(600);
+ digitalWrite(green,HIGH);
+ delay(700);
+ digitalWrite(green, HIGH);
+ }
  else {
   digitalWrite(green,LOW);
   digitalWrite(yellow, HIGH);
   digitalWrite(red, LOW);
+  
  }
  if 
-  (soil >= 425)
+  (soil >= 618)
   digitalWrite(red, HIGH);
    
-  delay(500);
+  delay(200);
   digitalWrite(red,LOW);
  
   
   
  
  
-  delay(500);
+  delay(300);
 }
