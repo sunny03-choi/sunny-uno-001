a6bd61cb3f9d5870f821283e17e3033c96bc5a72
sunwoo <62876483+sunny03-choi@users.noreply.github.com>
2020-05-15 22:17:29 +0900

    Update sketch_may06.ino

--- sketch_may06.ino
+++ sketch_may06.ino
@@ -1,5 +1,4 @@
 
-
 #define A0Pin 0
 #include<LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd (0x27,16,2);
@@ -17,7 +16,7 @@ void setup() {
   
  Serial.begin(9600);
  
-  pinMode (pump,OUTPUT);
+  pinMode (pump,OUTPUT);      
   pinMode (red, OUTPUT);
   pinMode (yellow, OUTPUT);
   pinMode (green, OUTPUT); 
@@ -26,17 +25,19 @@ void setup() {
 
 void loop() {
   
-  int soil = analogRead(A0);
-  lcd.init();
-  lcd.backlight();
+  int soil = analogRead(A0);     //토양 값을 A0 출력
+  lcd.init();                    //lcd 시작
+  lcd.backlight();               //lcd backlight 켬   
   Serial.print("read soil : ");
   Serial.println(soil);
   delay(900);
-  lcd.begin(16,2);
-  lcd.clear();
-  lcd.home();
-  lcd.print("read soil : ");
-  lcd.println(soil);
+  lcd.begin(16,2);               //lcd 시작   
+  lcd.clear();                  //lcd 초기화
+  lcd.home();                   //lcd (0.0)에 맞춤   
+  lcd.print("read soil : ");    //lcd "read soil"이라고 씀  
+  lcd.println(soil);           //lcd soil 값 출력
+  delay(1000);
+  
   
   if (soil >618) {
     Serial.println ("vary dry");
@@ -52,11 +53,11 @@ void loop() {
     digitalWrite(pump,LOW);
   }
   else {
-    Serial.println(" vary wet");
-    lcd. setCursor(0,1);
-    lcd.print(" Wet");
-    delay(300);
-    digitalWrite(pump, LOW);
+    Serial.println(" vary wet");   
+    lcd. setCursor(0,1);          //lcd 에 0,1에 글씨출력  
+    lcd.print(" Wet");            //lcd 에 wet 라고 출력   
+    delay(300);                   
+    digitalWrite(pump, LOW);      //pump를 끔
   }
  if (soil <470) {
  digitalWrite(green, HIGH);
@@ -68,9 +69,9 @@ void loop() {
  digitalWrite(green, HIGH);
  }
  else {
-  digitalWrite(green,LOW);
-  digitalWrite(yellow, HIGH);
-  digitalWrite(red, LOW);
+  digitalWrite(green,LOW);         //green led 끔   
+  digitalWrite(yellow, HIGH);      //yellow led 켬   
+  digitalWrite(red, LOW);          //red led 끔    
   
  }
  if 
