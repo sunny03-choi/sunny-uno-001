7e0d25ca924cc88f7e3fb75be037ae8e94ce88ad
sunwoo <62876483+sunny03-choi@users.noreply.github.com>
2020-05-18 22:50:26 +0900

    Update sketch_may06.ino

--- sketch_may06.ino
+++ sketch_may06.ino
@@ -8,6 +8,9 @@ int yellow = 10;
 int green = 6;
 int pump =8; 
 
+
+ 
+
 void setup() {
   
  Serial.begin(9600);
@@ -16,7 +19,14 @@ void setup() {
   pinMode (red, OUTPUT);
   pinMode (yellow, OUTPUT);
   pinMode (green, OUTPUT); 
-  }
+  
+
+
+}
+
+
+ 
+
 void loop() {
   
   int soil = analogRead(A0);     //토양 값을 A0 출력
@@ -24,26 +34,29 @@ void loop() {
   lcd.backlight();               //lcd backlight 켬   
   Serial.print("read soil : ");
   Serial.println(soil);
-  delay(900);
+  delay(1200);
   lcd.begin(16,2);               //lcd 시작   
   lcd.clear();                  //lcd 초기화
   lcd.home();                   //lcd (0.0)에 맞춤   
   lcd.print("read soil : ");    //lcd "read soil"이라고 씀  
   lcd.println(soil);           //lcd soil 값 출력
   
-   if (soil <400) {
+  
+ 
+ 
+  if (soil <400) {
    
  Serial.println("enough water");
  lcd.setCursor(0,1);
  lcd.println("enough");
  digitalWrite(green, HIGH);
- delay(4000);
+ delay(7000);
  digitalWrite(green,LOW);
  }
  if (soil <575) {
   Serial.println(" not enough");
  digitalWrite(yellow,HIGH);
-  delay(100);
+  delay(400);
   digitalWrite(yellow,LOW);
  }
 
@@ -52,20 +65,20 @@ void loop() {
     Serial.println ("vary dry");
     Serial.println ("Pumping 1 second");
     lcd.noDisplay();
-    delay(1100);
+    delay(1500);
     lcd.display();
     lcd.setCursor(0,1);
     lcd.println(" dry,Pumping");
-    delay(100);
+    delay(500);
     
    
   digitalWrite(red,HIGH);
  
     digitalWrite(pump,HIGH);
-    delay(3000);
+    delay(6000);
     digitalWrite(pump,LOW);
    digitalWrite(red,LOW);
   }
   
-delay(100);
+delay(400);
 }
